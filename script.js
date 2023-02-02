0205366eea3a7b184f795e6931e2f6eee3a04e70
Reuben Schmolke <sandwichcrashers@gmail.com>
2023-02-02 13:54:45 -0800

    added variables and empty array

--- script.js
+++ script.js
@@ -11,50 +11,19 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var passLength = window.prompt("Choose length between 8 & 128 characters");
-  var lowChar = window.prompt("Would you like lowercase letters?");
-  var upChar = window.prompt("Would you like uppercase letters?");
-  var numChar = window.prompt("Would you like numbers?");
-  var speChar = window.prompt("Would you like special characters?")
-  if(!passLength) {
-    return;
-  }else{
-    console.log(passLength);
-  }
-  if(!lowChar) {
-    return;
-  }if(lowChar=="no") {
-    return;
-  }else{
-    console.log(lowChar);
-  }
-  if(!upChar) {
-    return;
-  }if(upChar=="no") {
-    return;
-  }else{
-    console.log(upChar);
-  }
-  if(!numChar) {
-    return;
-  }if(numChar=="no") {
-    return;
-  }else{
-    console.log(numChar);
-  }
-  if(!speChar) {
-    return;
-  }if(speChar=="no") {
-    return;
-  }else{
-    console.log(speChar);
-  }
+  var lengthPrompt = window.prompt("Choose length between 8 & 128 characters");
+  var lowCharPrompt = window.prompt("Would you like lowercase letters?");
+  var upCharPrompt = window.prompt("Would you like uppercase letters?");
+  var numCharPrompt = window.prompt("Would you like numbers?");
+  var speCharPrompt = window.prompt("Would you like special characters?")
 
-  // console.log(passLength);
-  // console.log(lowChar);
-  // console.log(upChar);
-  // console.log(numChar);
-  // console.log(speChar);
+  var passMinLength = 8;
+  var passMaxLength = 128;
+  var upCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  var lowCharacters = "abcdefghijklmnopqrstuvwxyz".split("");
+  var numCharacters = "0123456789".split("");
+  var speCharacters = "!@#$%^&*()-_".split("");
+
 }
 
 // Add event listener to generate button
